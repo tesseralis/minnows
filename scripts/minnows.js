54a0c5e33d81fe0065c9726586d3036ede0f35e8
Nat Alison <tesseralis@gmail.com>
2016-08-06 21:23:45 -0700

    Add highlighting!

--- scripts/minnows.js
+++ scripts/minnows.js
@@ -2,16 +2,20 @@ const canvasLength = 700
 const ringRadiusBase = 18
 const blockSize = 8
 
-const svg = d3.select('body').append('svg')
-  .attr('width', canvasLength)
-  .attr('height', canvasLength)
+// Utility functions
+function sum(arr) { return arr.reduce((a, b) => a + b, 0); }
+function avg(...arr) { return sum(arr) / arr.length; }
 
 function ringRadius(gen) {
   return ringRadiusBase * Math.pow(gen, 1.75);
 }
 
+const svg = d3.select('body').append('svg')
+  .attr('width', canvasLength)
+  .attr('height', canvasLength)
+
 // Draw the minos for each generation
-function drawPolyominoes(element, polyominoes, links) {
+function drawPolyominoes(element, polyominoes, linkData) {
   const diagram = element.append('g').classed('diagram', true)
     .attr('transform', `translate(${canvasLength/2} ${canvasLength/2})`)
 
@@ -34,14 +38,13 @@ function drawPolyominoes(element, polyominoes, links) {
     return {radius, angle}
   }
 
-  function sum(arr) { return arr.reduce((a, b) => a + b, 0); }
-  function avg(...arr) { return sum(arr) / arr.length; }
   function avgAngle(a, b) {
     const x = Math.abs(a - b)
     const result = x < Math.PI ? (a + b)/2 : (a + b)/2 + Math.PI
     return result % (2*Math.PI)
   }
 
+  // Add a third value in between the edges so we have a curve
   function spline({source, target}) {
     const src = radiusAndAngle(source)
     const tgt = radiusAndAngle(target)
@@ -51,15 +54,16 @@ function drawPolyominoes(element, polyominoes, links) {
     return [src, {radius: midRad, angle: midAngle}, tgt]
   }
     
-  diagram.append('g').classed('links', true).selectAll('path.link')
-    .data(links.map(spline))
+  const links = diagram.append('g').classed('links', true)
+  const link = links.selectAll('path.link')
+    .data(linkData)
     .enter().append('path').classed('link', true)
-    .attr('d', curve)
+    .attr('d', d => curve(spline(d)))
 
   // Draw groups for each generation
   const generations = diagram.append('g').classed('generations', true)
     .attr('transform', 'rotate(-90)')
-    .selectAll('.generation')
+  const generation = generations.selectAll('.generation')
     .data(polyominoes)
     .enter().append('g')
     .classed('generation', true)
@@ -75,10 +79,10 @@ function drawPolyominoes(element, polyominoes, links) {
     return `translate(${x} ${y})`
   }
 
-  const minos = generations.selectAll('.mino')
+  const minoWrapper = generation.selectAll('.mino-wrapper')
     .data(generation => generation)
     .enter().append('g')
-    .classed('mino', true)
+    .classed('mino-wrapper', true)
     .attr('transform', transform)
 
   // Center the mino on 0,0
@@ -89,11 +93,25 @@ function drawPolyominoes(element, polyominoes, links) {
     return `translate(${translate(xAvg)} ${translate(yAvg)})`
   }
 
-  const minoWrappers = minos.append('g').classed('mino-wrapper', true)
+  const mino = minoWrapper.append('g').classed('mino', true)
     .attr('transform', translateMino)
+    .on('mouseover', function(d, i) {
+      d3.select(this).selectAll('.block').classed('isFocused', true)
+
+      const gen = d.length - 1
+
+      link.classed('isFocused', ({source, target}) => {
+        return source[0] === gen && source[1] === i ||
+               target[0] === gen && target[1] === i
+      })
+    })
+    .on('mouseout', function(d, i) {
+      d3.select(this).selectAll('.block').classed('isFocused', false)
+      link.classed('isFocused', false)
+    })
 
   // Draw the squares on each polyomino
-  minoWrappers.selectAll('rect.block')
+  const block = mino.selectAll('rect.block')
     .data(mino => mino)
     .enter().append('rect').classed('block', true)
     .attr('width', blockSize)
